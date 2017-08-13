13b8ed75759c50610edbaa42c9080562c9366dae
Gabriel Al-Ghalith <GabeAl@users.noreply.github.com>
2017-08-12 23:45:01 -0500

    v1.5a: Mirrored space-semicolon changes
    Apparently they got buried in a different fork!

--- itree.c
+++ itree.c
@@ -297,19 +297,21 @@ inline long xeTreeU_GG(KMerY *tree, WTYPE word, IXTYPE ix, UTree *ut) {
 		//printf("old = %s\nnew = %s\n",oldOrig,ut->SampStrings[ix]);
 		//printf("--> Conserved num semis = %u, position = %u\n",numP, ixP);
 		if (numP == 0) // kingdom diverged
-			++numP, strcpy(buffer,"k__; "), ixP = 2; 
+			++numP, strcpy(buffer,"k__;"), ixP = 2; 
 		if (numP == 1)
-			++numP, strcpy(buffer + ixP + 2,"p__; "), ixP += 5;
+			++numP, strcpy(buffer + ixP + 1,"p__;"), ixP += 4;
 		if (numP == 2)
-			++numP, strcpy(buffer + ixP + 2,"c__; "), ixP += 5;
+			++numP, strcpy(buffer + ixP + 1,"c__;"), ixP += 4;
 		if (numP == 3)
-			++numP, strcpy(buffer + ixP + 2,"o__; "), ixP += 5;
+			++numP, strcpy(buffer + ixP + 1,"o__;"), ixP += 4;
 		if (numP == 4)
-			++numP, strcpy(buffer + ixP + 2,"f__; "), ixP += 5;
+			++numP, strcpy(buffer + ixP + 1,"f__;"), ixP += 4;
 		if (numP == 5)
-			++numP, strcpy(buffer + ixP + 2,"g__; "), ixP += 5;
-		if (numP == 6) // species diverged
-			strcpy(buffer + ixP + 2,"s__");
+			++numP, strcpy(buffer + ixP + 1,"g__;"), ixP += 4;
+		if (numP == 6)
+			++numP, strcpy(buffer + ixP + 1,"s__;"), ixP += 4;
+		if (numP == 7) // strain diverged
+			strcpy(buffer + ixP + 1,"t__");
 		//printf("--> Interpolated: %s\n",buffer);
 		IXTYPE new_ix = addSampleUd(ut,buffer);
 		
@@ -392,19 +394,21 @@ inline int parapluieU(UTree *ktree, WTYPE wordO, IXTYPE ix) {
 	strcpy(buffer,oldOrig); \
 	if (numP < 2) { tree->ix = BAD_IX; return 1; } \
 	if (numP == 0) \
-		++numP, strcpy(buffer,"k__; "), ixP = 2; \
+		++numP, strcpy(buffer,"k__;"), ixP = 2; \
 	if (numP == 1) \
-		++numP, strcpy(buffer + ixP + 2,"p__; "), ixP += 5; \
+		++numP, strcpy(buffer + ixP + 1,"p__;"), ixP += 4; \
 	if (numP == 2) \
-		++numP, strcpy(buffer + ixP + 2,"c__; "), ixP += 5; \
+		++numP, strcpy(buffer + ixP + 1,"c__;"), ixP += 4; \
 	if (numP == 3) \
-		++numP, strcpy(buffer + ixP + 2,"o__; "), ixP += 5; \
+		++numP, strcpy(buffer + ixP + 1,"o__;"), ixP += 4; \
 	if (numP == 4) \
-		++numP, strcpy(buffer + ixP + 2,"f__; "), ixP += 5; \
+		++numP, strcpy(buffer + ixP + 1,"f__;"), ixP += 4; \
 	if (numP == 5) \
-		++numP, strcpy(buffer + ixP + 2,"g__; "), ixP += 5; \
+		++numP, strcpy(buffer + ixP + 1,"g__;"), ixP += 4; \
 	if (numP == 6) \
-		strcpy(buffer + ixP + 2,"s__"); \
+		++numP, strcpy(buffer + ixP + 1,"s__;"), ixP += 4; \
+	if (numP == 7) \
+		strcpy(buffer + ixP + 1,"t__"); \
 	IXTYPE new_ix = addSampleUd(ut,buffer); \
 	tree->ix = new_ix; \
 	return 0; \
